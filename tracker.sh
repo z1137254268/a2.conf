4a9779cfc0e10ffeea3ff1026b6f2005462f592a
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2021-10-16 05:56:00 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -53,9 +53,9 @@ GET_TRACKERS() {
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers..."
         TRACKER=$(
             ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
+            ${DOWNLOADER} https://cdn.jsdelivr.net/gh/DeSireFire/animeTrackerList/ATaria2_all.txt ||
                 ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection@master/all_aria2.txt ||
-                ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt ||
-                ${DOWNLOADER} https://cdn.jsdelivr.net/gh/DeSireFire/animeTrackerList/ATaria2_all.txt
+                ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt
         )
     else
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers from url(s):${CUSTOM_TRACKER_URL} ..."
